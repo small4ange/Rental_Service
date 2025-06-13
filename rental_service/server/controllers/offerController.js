1eaef91f68d85264e90e77c86f2127a3ce60d9f6
small4ange <katrinavolk37695@gmail.com>
2025-06-13 10:59:49 +0300

    feat: Добавить маршрутизацию и связь с базой данных

---
+++ rental_service/server/controllers/offerController.js
@@ -0,0 +1,13 @@
+import { Offer } from "../models/offer.js";
+
+// возвращает все предложения из таблицы Offer
+async function getAllOffers(req, res, next) {
+    try {
+        const offers = await Offer.findAll();
+        res.send(offers);
+    } catch (error) {
+        console.error('Не удалось получить список предложений:', error);
+    }
+}
+
+export {getAllOffers};
